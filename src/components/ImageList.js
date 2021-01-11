5b39601f810afed659f9d0cc2c3b93fefe5d5df0
Anurag Das <anurag.das23.ad@gmail.com>
2021-01-11 15:16:44 -0500

    Added key to the root element in children component

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 const ImageList = (props) => {
     
-    const imageTags = props.images.map(image => {
-        return <img alt='' src={image.urls.regular}></img>
+    const imageTags = props.images.map(({ description, urls, id }) => {
+        // Adding key to the root element we are returning 
+        return <img alt={description} src={urls.regular} key={id}></img>
     });
 
     return (
